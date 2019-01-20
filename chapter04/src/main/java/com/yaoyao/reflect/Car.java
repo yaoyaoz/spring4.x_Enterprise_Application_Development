66903ad86f1f1525c517f329e333b6f8e32cc67c
yaoyaoz <905440208@qq.com>
2019-01-20 15:56:55 +0800

    chapter04：反射

---
+++ chapter04/src/main/java/com/yaoyao/reflect/Car.java
@@ -0,0 +1,55 @@
+package com.yaoyao.reflect;
+
+/**
+ * Car类：两个构造函数、一个方法、3个属性
+ *
+ * Created by yaoyao on 2019-01-20.
+ */
+public class Car {
+
+    private String brand;
+
+    private String color;
+
+    private int maxSpeed;
+
+    //默认构造函数
+    public Car() {
+    }
+
+    //带参构造函数
+    public Car(String brand, String color, int maxSpeed) {
+        this.brand = brand;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+    }
+
+    //未带参的方法
+    public void introduce()  {
+        System.out.println("brand:" + brand + "; color:"+ color + "; maxSpeed:" + maxSpeed);
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getMaxSpeed() {
+        return maxSpeed;
+    }
+
+    public void setMaxSpeed(int maxSpeed) {
+        this.maxSpeed = maxSpeed;
+    }
+}
---
+++ chapter04/src/main/java/com/yaoyao/reflect/ReflectTest.java
@@ -0,0 +1,50 @@
+package com.yaoyao.reflect;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
+/**
+ * 通过Java反射机制以一种间接的方式操控目标类（com.yaoyao.reflect.Car）
+ * <p>
+ * 传统方式直接调用目标类：
+ * Car car = new Car();
+ * car.setBrand("红旗CA72");
+ * 或者：
+ * Car car = new Car("hongqiCA72", "黑色", 150);
+ * <p>
+ * Created by yaoyao on 2019-01-20.
+ */
+public class ReflectTest {
+
+    public static Car initByDefaultConst() throws Throwable {
+
+        //1、通过类装载器获取Car类对象
+        ClassLoader loader = Thread.currentThread().getContextClassLoader();
+        Class clazz = loader.loadClass("com.yaoyao.reflect.Car");
+
+        //2、获取类的默认构造函数对象并通过它实例化Car
+        Constructor cons = clazz.getDeclaredConstructor((Class[]) null);
+        //问题：这里为什么要用Class[]呢？不加[]，运行的时候就会报错：
+        //Exception in thread "main" java.lang.NoSuchMethodException: com.yaoyao.reflect.Car.<init>(null)
+        Car car = (Car) cons.newInstance();
+
+        //3、通过反射方法设置属性
+        Method setBrand = clazz.getMethod("setBrand", String.class);
+        setBrand.invoke(car, "红旗CA72");
+        Method setColor = clazz.getMethod("setColor", String.class);
+        setColor.invoke(car, "黑色");
+        Method setMaxSpeed = clazz.getMethod("setMaxSpeed", int.class);
+        setMaxSpeed.invoke(car, 200);
+        return car;
+    }
+
+    public static void main(String[] args) throws Throwable {
+        Car car = initByDefaultConst();
+        car.introduce();
+    }
+    /**
+     输出：
+     brand:红旗CA72; color:黑色; maxSpeed:200
+     */
+
+}
